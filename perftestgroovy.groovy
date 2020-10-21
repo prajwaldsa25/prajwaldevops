81d9a8127d747aaf639a0819350836d46e5cb62f
prajwaldsa25 <prajwalagnel@gmail.com>
2020-10-21 12:22:51 +1100

    updated groovy

--- perftestgroovy.groovy
+++ perftestgroovy.groovy
@@ -1,5 +1,5 @@
 node
 {
 stage 'Run JMter Test'
-sh /home/prajwal/Documents/apache-jmeter-5.3/bin/jmeter.sh -Jjmeter.save.saveservice.output_format=xml -n -t /home/prajwal/Documents/mytest.jmx -l /home/prajwal/Documents/mytest_jenkins_git1.jtl
+sh /home/prajwal/Documents/apache-jmeter-5.3/bin/jmeter.sh -n -t /home/prajwal/Documents/mytest.jmx -l /home/prajwal/Documents/mytest_jenkins_git1.jtl
 }
