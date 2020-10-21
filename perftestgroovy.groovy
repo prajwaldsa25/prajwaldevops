1deb9744a026fe7beb4a5fd697c2b8677e865a72
prajwaldsa25 <prajwalagnel@gmail.com>
2020-10-21 12:37:36 +1100

    updated groovy

--- perftestgroovy.groovy
+++ perftestgroovy.groovy
@@ -1,4 +1,4 @@
 node{
 stage 'Run JMeter Test'
-sh '/home/prajwal/Documents/apache-jmeter-5.3/bin/jmeter.sh -n -t /home/prajwal/Documents/mytest.jmx -l /home/prajwal/Documents/mytest_jenkins_git3.jtl'
+sh '/home/prajwal/Documents/apache-jmeter-5.3/bin/jmeter.sh -n -t /home/prajwal/Documents/mytest.jmx -l /home/prajwal/Documents/mytest_jenkins_git333.jtl'
 }
